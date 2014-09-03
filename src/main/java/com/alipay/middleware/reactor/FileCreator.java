ab5f9817374c8e0db41129f83a5f357966a35997
unknown <mingze.xmz@ali-73310n.hz.ali.com>
2014-09-04 06:00:46 +0800

    remove non utf8 chars

--- src/main/java/com/alipay/middleware/reactor/FileCreator.java
+++ src/main/java/com/alipay/middleware/reactor/FileCreator.java
@@ -34,13 +34,10 @@ public class FileCreator {
 	        String val = "";  
 	        Random random = new Random();  
 	          
-	        //参数length，表示生成几位随机数  
 	        for(int i = 0; i < random.nextInt(200); i++) {  
 	              
 	            String charOrNum = random.nextInt(2) % 2 == 0 ? "char" : "num";  
-	            //输出字母还是数字  
 	            if( "char".equalsIgnoreCase(charOrNum) ) {  
-	                //输出是大写字母还是小写字母  
 	                int temp = random.nextInt(2) % 2 == 0 ? 65 : 97;  
 	                val += (char)(random.nextInt(26) + temp);  
 	            } else if( "num".equalsIgnoreCase(charOrNum) ) {  
