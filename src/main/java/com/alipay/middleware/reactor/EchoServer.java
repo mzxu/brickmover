ca7a16d556c8c8c0772057cec45415aece41a0a7
unknown <mingze.xmz@ali-73310n.hz.ali.com>
2014-09-04 06:14:19 +0800

    fix uncompatiable of epoll

--- src/main/java/com/alipay/middleware/reactor/EchoServer.java
+++ src/main/java/com/alipay/middleware/reactor/EchoServer.java
@@ -29,6 +29,7 @@ import io.netty.channel.ChannelOption;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.epoll.EpollServerSocketChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -76,7 +77,7 @@ public final class EchoServer {
          try {
             ServerBootstrap b = new ServerBootstrap();
             b.group(BossEventLoopGroup, WorkerEventLoopGroup)
-                .channel(NioServerSocketChannel.class)
+                .channel(EpollServerSocketChannel.class)
                 .childOption(ChannelOption.TCP_NODELAY, true)
                 .childOption(ChannelOption.SO_KEEPALIVE, true)
                 .childOption(ChannelOption.SO_REUSEADDR, true)
